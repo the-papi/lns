4104869b8de97e7e584f2b5eeb7f13a2ec2c4fdc
Patrik Janoušek <patrikjanousek97@gmail.com>
2020-11-06 13:09:43 +0100

    Testing failing test

--- symlinker/tests.py
+++
@@ -1,7 +0,0 @@
-from django.test import TestCase
-
-
-class RandomTestCase(TestCase):
-    def first_test(self):
-        self.assertEqual("a", "a")
-        self.assertEqual("a", "b")
---
+++ symlinker/tests/test_first.py
@@ -0,0 +1,7 @@
+from django.test import TestCase
+
+
+class AnimalTestCase(TestCase):
+    def test_first(self):
+        self.assertEqual('a', 'a')
+        self.assertEqual('a', 'b')
